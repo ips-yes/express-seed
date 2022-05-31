c8d782c72ae127610749e5a5ae567f4c05e17b37
Alexander-Wang-IPS <106104731+Alexander-Wang-IPS@users.noreply.github.com>
2022-05-31 09:06:53 -0400

    Cut out unneeded installs in dockerfile

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -4,9 +4,7 @@ import * as dev from './dev.json';
 import * as stage from './stage.json';
 import * as docker from './docker.json';
 
-const isdocker = isDocker();
-
 const seedEnvironment = process.env.SEED_ENVIRONMENT || 'dev';
-const config: IConfig = isdocker ? docker : seedEnvironment === 'dev' ? dev : stage;
+const config : IConfig = isDocker() ? docker : seedEnvironment === 'dev' ? dev : stage; 
 
 export default config;
