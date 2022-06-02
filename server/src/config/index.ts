e8df92b2bfe5e59856bca20efeae11160ad867b5
Alexander-Wang-IPS <106104731+Alexander-Wang-IPS@users.noreply.github.com>
2022-06-02 13:50:25 -0400

    Updated compose.yaml file, moved docker config to server/config and added proper mounting in docker compose

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,7 +2,7 @@ import isDocker from 'is-docker';
 import { IConfig } from './IConfig';
 import * as dev from './dev.json';
 import * as stage from './stage.json';
-import * as docker from './docker.json';
+import * as docker from '../../config/docker.json';
 
 const seedEnvironment = process.env.SEED_ENVIRONMENT || 'dev';
 const config : IConfig = isDocker() ? docker : seedEnvironment === 'dev' ? dev : stage; 
