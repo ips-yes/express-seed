fa76fd5abf1a92d43f6c0a641c2ce754cf2566a8
Alexander-Wang-IPS <106104731+Alexander-Wang-IPS@users.noreply.github.com>
2022-06-14 15:12:11 -0400

    Fixed sending a status code inconsistent with the json response body

--- server/src/middleware/CheckProtocol.ts
+++ server/src/middleware/CheckProtocol.ts
@@ -4,7 +4,7 @@ import logger from '../utils/Logger';
 const checkProtocol = async (req, res, next) => {
     if(req.protocol != 'https'){
         logger.info('User sent HTTPS only request over HTTP');
-        return res.status(401).json(constants.AUTH.BAD_PROTOCOL);
+        return res.status(403).json(constants.AUTH.BAD_PROTOCOL);
     }
 
     return next();
