6c09d3070e86dc7018fea1e2b613f61a72e53c30
Alexander-Wang-IPS <106104731+Alexander-Wang-IPS@users.noreply.github.com>
2022-07-05 11:25:36 -0400

    Middleware now properly enters a disconnected state when a disconnect is detected

--- server/src/middleware/DatabaseHealth.ts
+++ server/src/middleware/DatabaseHealth.ts
@@ -4,30 +4,51 @@ const ping = databasePing.default;
 
 const databaseHealth = async (req, res, next) => {
   if (ping.databaseConnected) {
-    try {
+    const { end } = res;
+
+    res.end = function (a, b) {
+      if (res.statusCode >= 500) {
+        ping.databaseConnected = false;
+      }
+
+      res.end = end;
+      res.end(a, b);
+    };
+
+    if (ping.databaseConnected) {
       return next();
-    } catch {
-      ping.databaseConnected = false;
-      return res.status(500).json('Database could not be found. Please try again later.');
     }
-  } else {
-    return ping.getPromise().then((online) => {
-      if (online) {
-        try {
-          return next();
-        } catch {
+
+    ping.databaseConnected = false;
+    return res.status(500).json('Database could not be found. Please try again later.');
+  }
+  return ping.getPromise().then((online) => {
+    if (online) {
+      const { end } = res;
+
+      res.end = function (a, b) {
+        if (res.statusCode >= 500) {
           ping.databaseConnected = false;
-          return res.status(500).json('Database could not be found. Please try again later.');
         }
+
+        res.end = end;
+        res.end(a, b);
+      };
+
+      if (ping.databaseConnected) {
+        return next();
       }
 
       ping.databaseConnected = false;
       return res.status(500).json('Database could not be found. Please try again later.');
-    }).catch(() => {
-      ping.databaseConnected = false;
-      return res.status(500).json('Database could not be found. Please try again later.');
-    });
-  }
+    }
+
+    ping.databaseConnected = false;
+    return res.status(500).json('Database could not be found. Please try again later.');
+  }).catch(() => {
+    ping.databaseConnected = false;
+    return res.status(500).json('Database could not be found. Please try again later.');
+  });
 };
 
 export default databaseHealth;
