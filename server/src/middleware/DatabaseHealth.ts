a036c9c0fc9ef40ed61d3cf04c9ab48bcea0c8bb
Alexander-Wang-IPS <106104731+Alexander-Wang-IPS@users.noreply.github.com>
2022-07-05 11:50:10 -0400

    Added documentation

--- server/src/middleware/DatabaseHealth.ts
+++ server/src/middleware/DatabaseHealth.ts
@@ -3,10 +3,11 @@ import * as databasePing from '../utils/DatabasePing';
 const ping = databasePing.default;
 
 const databaseHealth = async (req, res, next) => {
-  if (ping.databaseConnected) {
+  // if the server thinks it's already connected, confirm the response is good, and then send it through.
+  if (ping.databaseConnected) { 
     const { end } = res;
 
-    res.end = function (a, b) {
+    res.end = function (a, b) { // This checks the status code of the response, to check if a disconnect has occured.
       if (res.statusCode >= 500) {
         ping.databaseConnected = false;
       }
@@ -15,18 +16,19 @@ const databaseHealth = async (req, res, next) => {
       res.end(a, b);
     };
 
-    if (ping.databaseConnected) {
+    if (ping.databaseConnected) { // connected and good to go
       return next();
     }
 
-    ping.databaseConnected = false;
+    ping.databaseConnected = false; // not actually connected, send an error!
     return res.status(500).json('Database could not be found. Please try again later.');
   }
-  return ping.getPromise().then((online) => {
-    if (online) {
+  // If the database isnt connected, ping it to try and connect!
+  return ping.getPromise().then((online) => { 
+    if (online) { // Connection was successful, but lets check to make sure
       const { end } = res;
 
-      res.end = function (a, b) {
+      res.end = function (a, b) { // This checks the status code of the response, to check if a disconnect has occured.
         if (res.statusCode >= 500) {
           ping.databaseConnected = false;
         }
@@ -35,18 +37,18 @@ const databaseHealth = async (req, res, next) => {
         res.end(a, b);
       };
 
-      if (ping.databaseConnected) {
+      if (ping.databaseConnected) { // connected and good to go
         return next();
       }
 
-      ping.databaseConnected = false;
+      ping.databaseConnected = false; // not actually successful, send an error!
       return res.status(500).json('Database could not be found. Please try again later.');
     }
 
-    ping.databaseConnected = false;
+    ping.databaseConnected = false; // (re)connect not successful, send an error!
     return res.status(500).json('Database could not be found. Please try again later.');
   }).catch(() => {
-    ping.databaseConnected = false;
+    ping.databaseConnected = false; // something went wrong trying to check for connection, send an error!
     return res.status(500).json('Database could not be found. Please try again later.');
   });
 };
--- server/src/utils/DatabasePing.ts
+++ server/src/utils/DatabasePing.ts
@@ -7,7 +7,8 @@ const DB_PARAMS = config.db;
 export default class DatabasePing {
   static instance;
 
-  static databaseConnected = false;
+  /// if the server thinks the database is connected or not
+  static databaseConnected = false; 
 
   static isSearching;
 
@@ -16,7 +17,7 @@ export default class DatabasePing {
   static attemptConnect = async () : Promise<boolean> => {
     DatabasePing.isSearching = true;
 
-    try {
+    try { // attempt to connect to and set up the database
       await db.sequelize.authenticate();
       if (DB_PARAMS.SYNC) {
         await db.sequelize.sync(); // If this is enabled in config.json it will sync the DB to the code
@@ -30,7 +31,7 @@ export default class DatabasePing {
         DatabasePing.databaseConnected = true;
         logger.info('Database connected!');
       }
-    } catch (e) {
+    } catch (e) { // connection or setup failed, we are not connected to the database!
       logger.warn(`Database configuration failed due to: ${e.message}`);
       DatabasePing.databaseConnected = false;
     }
@@ -39,14 +40,14 @@ export default class DatabasePing {
     return DatabasePing.databaseConnected;
   }
 
-  static getInstance() : DatabasePing {
+  static getInstance() : DatabasePing { // singleton instance getter
     if (!DatabasePing.instance) {
       DatabasePing.instance = new DatabasePing();
     }
     return DatabasePing.instance;
   }
 
-  static getPromise() : Promise<boolean> {
+  static getPromise() : Promise<boolean> { // gets a promise to return if the DB is connected or not
     if (DatabasePing.isSearching) {
       return DatabasePing.promise;
     }
